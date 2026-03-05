1535b334919c31e0c1d12d92584656aacf91d477
Rasmus <rame0001@stud.kea.dk>
2026-03-05 09:26:00 +0100

    feat: Refactor signup process and remove unused templates; add user validation

--- app.py
+++ app.py
@@ -3,153 +3,45 @@ import x
 import uuid
 import time
 from flask_session import Session
- 
+
 from icecream import ic
-ic.configureOutput(prefix=f'___________ | ', includeContext=True)
- 
+ic.configureOutput(prefix=f'______ | ', includeContext=True)
+
 app = Flask(__name__)
- 
+
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
-
 
 ##############################
 @app.get("/signup")
 def show_signup():
     try:
-        return render_template("page_signup.html", x=x)
-    except Exception as ex:
-        print(ex, flush = True)
-        return "ups ..."
- 
-##############################
-@app.post("/api-check-username")
-def check_username():
-    try:
-        user_username = x.validate_user_username()
-        db, cursor = x.db()
-        q = "SELECT * FROM users WHERE user_username = %s"
-        cursor.execute(q, (user_username,))
-        row = cursor.fetchone()
-        if not row:
-            return f"""
-                <browser mix-update="span">
-                    Username available
-                </browser>
-            """
-        
-        return f"""
-            <browser mix-update="span">
-                Username taken
-            </browser>
-        """        
- 
- 
-    except Exception as ex:
-        # print(ex, flush = True)
-        ic(ex)
- 
-        if "--error-- user_username" in str(ex):
-            return f"""<browser mix-update="span">{ex.args[0]}</browser>""", 400
- 
-    
-        # Worst case, something unexpected
-        return f"""<browser mix-update="span">System under maintenance</browser>""", 500
- 
-    finally:
-        if "cursor" in locals(): cursor.close()
-        if "db" in locals(): db.close()  
- 
- 
- 
-##############################
-@app.post("/api-create-user")
-def create_user():
-    try:
-        user_username = x.validate_user_username()
-        user_first_name = x.validate_user_first_name()
-        user_pk = uuid.uuid4().hex
-        user_create_at = int(time.time())
-        db, cursor = x.db()
-        q = "INSERT INTO users VALUES(%s, %s, %s, %s)"
-        cursor.execute(q, (user_pk, user_username, user_first_name, user_create_at))
-        db.commit()
-
-        tip = render_template("___tip.html", message = "User created", status="ok")
- 
-        return f"""
-        <browser mix-update="#tooltip">{tip}</browser>
-        """
-        
-    
- 
-    except Exception as ex:
-        # print(ex, flush = True)
-        ic(ex)
- 
-        if "--error-- user_username" in str(ex):
-            return f"""<browser mix-update="span">{ex.args[0]}</browser>""", 400
- 
-        if "--error-- user_first_name" in str(ex):
-            tip = render_template("___tip.html", message="Invalid first name", status="error")
-            return f"""<browser mix-update="#tooltip">{tip}</browser>""", 400
- 
-        if "Duplicate entry" in str(ex) and "user_username" in str(ex):
-            return f"""<browser mix-update="span">Username already taken</browser>""", 400
- 
-        # Worst case, something unexpected
-        return f"""<browser mix-update="span">System under maintenance</browser>""", 500
- 
-    finally:
-        if "cursor" in locals(): cursor.close()
-        if "db" in locals(): db.close()
-
-##############################
-@app.get("/login")
-def login():
-    try:
-        return render_template("page_login.html")
-    except Exception as ex:
-        ic(ex)
-        return "ups", 500
-
-##############################
-@app.post("/login")
-def user_login():
-    try:
-        # TODO: Validate
-        # TODO: Check if email and password exist in the db
-        user = {
-            "name":"Rasmus"
-        }
-        session["user"] = user
-        return "You are logged in"
-    except Exception as ex:
-        ic(ex)
-        return str(ex)
-    finally:
-        if "cursor" in locals(): cursor.close()
-        if "db" in locals(): db.close()
-
-##############################
-@app.get("/profile")
-@x.no_cache # decorator
-def profile():
-    try:
         user = session.get("user", "")
-        if not user: return redirect("/login")
-        ic(user)
-        return render_template("page_profile.html", user=user)
-    except Exception as ex:
-        ic(ex)
-        return "error"
-
-##############################
-@app.get("/logout")
-def logout():
-    try:
-        session.clear()
-        return redirect("/login")
+        return render_template("page_signup.html", user=user)
     except Exception as ex:
         ic(ex)
         return "ups"
+
+##############################
+@app.post("/api-create-user")
+def api_create_user():
+    try:
+        user_first_name = x.validate_user_first_name()
+        user_last_name = x.validate_user_last_name()
+    except Exception as ex:
+        ic(ex)
+
+        if "company_exception user_first_name" in str(ex):
+            error_message = f"user first name {x.USER_FIRST_NAME_MIN} to {x.USER_FIRST_NAME_MAX} characters"
+            ___tip = render_template("___tip.html", status="error", message=error_message)
+            return f"""<browser mix-after-begin="#tooltip">{___tip}</browser>"""
+
+        if "company_exception user_last_name" in str(ex):
+            error_message = f"user last name {x.USER_LAST_NAME_MIN} to {x.USER_LAST_NAME_MAX} characters"
+            ___tip = render_template("___tip.html", status="error", message=error_message)
+            return f"""<browser mix-after-begin="#tooltip">{___tip}</browser>"""
+
+        return "ups"
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
